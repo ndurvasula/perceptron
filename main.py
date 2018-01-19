fa67e701d31e44ff3775d484a92652799f69fedc
Qevian <qevian2000@gmail.com>
2018-01-19 10:35:48 -0500

    Added in comments and fixed the backprop...

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
 
 def main():
-	size = 10
-	lr = .1
-	num_data = 10
-	data_size = 1
+    size = 10 # size of layer or number of neurons; 
+	lr = .1 # learning rate; tells us how quickly we want our boundary to adjust itself 
+	num_data = 10 # number of data points we can train on
+	data_size = 1 # how many features each data point has
 
+    # Creating a layer of <size> neurons
 	layer1 = []
 	for i in range(size):
 		bias = random.random()
--- neuron.py
+++ neuron.py
@@ -8,14 +8,19 @@ class Neuron:
             return 0
         return 1
 
+    # 
     def n_apply(self, in_vec):
         accum = self.bias
         for i in xrange(len(in_vec)):
             accum += in_vec[i] * self.weights[i]
 
         return self.__activation(accum)
-
-    def adjust(self, ins, delta, lr):
+    
+    # adjust neuron based on error
+    # inp is list of inputs to this neuron
+    # partial is the partial derivative of error with respect to weighted input
+    # lr is learning rate
+    def adjust(self, inp, partial, lr):
         for i in xrange(len(ins)):
-            self.weights[i] += ins[i] * lr
-        self.bias += lr
+            self.weights[i] += inp[i] * partial * lr
+        self.bias += partial * lr
